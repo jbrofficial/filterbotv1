0701e2742522232b6225d50409ed15245f294217
JBR official <85146090+jbrofficial@users.noreply.github.com>
2021-08-13 20:52:24 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -1,48 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) @AlbertEinsteinTG
-
+# (c) @DalinMathew
+ 
 class Translation(object):
     
-    START_TEXT = """<b><u>Hey {} How are you!!</b></u>
-<i>{first}How can I help you..😊 \n made by #JBR❤️ 😜 \n Bₑ ₕₐₚₚY 💞<\b><\u> Enjoy your Life 😈 own way🦋🥳
+    START_TEXT = """<b><u>Hey {} !! </u>☺️
+<i>How can I help you..😊 \n made by #JBR❤️ 😜 \n Bₑ ₕₐₚₚY 💞<\b><\u> Enjoy your Life 😈 own way🦋🥳
 MAD BY @JBRofficial<\a>
-Any help contact me
+Any help contact !😎 </b>
+ 
+ 
 </i>"""    
-   
+    
     HELP_TEXT = """
-<b><i><u>How To Use Me!?</u></i></b>
-
-<i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
-</i>
-
-<b>Bot Commands (Works Only In Groups) :</b>
-
-    -> <code>/add chat_id</code>
-                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
-     <code>/add @Username</code>
-     
-    -> <code>/del chat_id</code>
-                OR                  - To disconnect A Group With A Channel
-     <code>/del @Username</code>
-     
-    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
-    
-    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
-
-            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
-            
-            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
-
-            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
-            
-            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
-            
-@jbrofficial 😻
+<b>Create Your Auto Filter Using This Link \n:- https://youtu.be/qW9o9G7stCk ☺️☺️ </b>
+ 
 """
-    
     ABOUT_TEXT = """<b>➥ Name</b> : <code>JBRMoviemaster</code>
     
 <b>➥ Creator</b> : <b><i><a href="https://t.me/jbrofficial">JBR__CREATION</b>

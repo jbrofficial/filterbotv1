f9fbd4286fbd305b17ba519c15ca341644f9fcc6
JBR official <85146090+jbrofficial@users.noreply.github.com>
2021-08-13 21:07:40 +0530

    Add files via upload

---
+++ bot/translation.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @DalinMathew
+
+class Translation(object):
+    
+    START_TEXT = """<b><u>Hey {} !! </u>☺️
+<i>How can I help you..😊 \n made by #JBR❤️ 😜 \n Bₑ ₕₐₚₚY 💞<\b><\u> Enjoy your Life 😈 own way🦋🥳
+MAD BY @JBRofficial<\a>
+
+any help contact me!!!😎 </b>
+
+ 
+</i>"""    
+    
+    HELP_TEXT = """
+<b>any questions ask Using This Link \n:- https://t.me/jbrofficial ☺️☺️ </b>
+
+"""
+    
+    ABOUT_TEXT = """<b>➥ <b>➥ Creator</b> : <b><i><a href="https://t.me/jbrofficial">JBR__CREATION</b>
+<b>➥ Language</b> : <code>Python3</code>
+<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+<b>➥ SCode</b> : <i><a href="https://github.com">ITS PERSONAL 💻</a></i>
+<b>➥ Subscribe ❤️</b> : <i><a href="https://youtube.com/channel/UC-43NwS9v4S32l4tS9GxYWg">
+"""">
+"""
+""">
+"""i>
+"""
+7stCk">Click Me</a></i>
+"""
+>
+
+<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+
+<b>➥ Deploy Your Own</b> : <i><a href="https://youtu.be/qW9o9G7stCk">Click Me</a></i>
+"""

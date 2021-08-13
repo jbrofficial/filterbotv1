090496e5c1bcc9a67109eda5e41489667b4b6d65
JBR official <85146090+jbrofficial@users.noreply.github.com>
2021-08-14 00:06:18 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,8 +5,8 @@
 class Translation(object):
     
     START_TEXT = """<b><u>Hey {} !! </u>☺️
-<i>ᴀᴍ ᴊᴜsᴛ ᴀ ᴀᴅᴠᴀɴᴄᴇ ᴀᴜᴛᴏ ғɪʟᴛᴇʀ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ᴛᴏ ᴡᴏʀᴋ ɪɴ <i>𝕄𝕒𝕝𝕒𝕪𝕒𝕝𝕒𝕞 ℝ𝕠𝕔𝕜𝕖𝕣𝕤 𝔾𝕣𝕠𝕦𝕡. 😉.</i>
-𝙔𝙤𝙪 𝘾𝙖𝙣'𝙩 𝘼𝙙𝙙 𝙈𝙚 𝙩𝙤 𝙔𝙤𝙪𝙧 𝙂𝙧𝙤𝙪𝙥!! 🍒
+<i>How can I help you..😊 \n made by #JBR❤️ 😜 \n Bₑ ₕₐₚₚY 💞</u> Enjoy your Life 😈 own way🦋🥳
+MAD BY @JBRofficial !! 🍒
 
 You can create Your own by Clicking on Deploy Tutorial !!!😎 </b>
 
